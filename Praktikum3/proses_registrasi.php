a397bb80c37109c64ae3309561c745c288b972f4
halizahhhh <halizahhhh@gmail.com>
2023-03-10 15:45:19 +0700

    Praktikum2

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,21 @@
+<?php 
+    $domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+    $program_studi = [
+        "SI" => "Sistem Informasi",
+        "TI" => "Teknik Informatika",
+        "BD" => "Bisnis Digital"
+    ];
+
+    $skills = [
+        "HTML"=>10,
+        "CSS"=>10, 
+        "JavaScrip"=>20,
+        "RWD Bootstrap"=>20, 
+        "PHP"=>30, 
+        "Python"=>30, 
+        "JAVA"=>50
+    ];
+
+   
+?>
