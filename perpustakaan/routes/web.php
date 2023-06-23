9412fbbfb64371d6e152b33b5d822094bcaca586
halizahhhh <halizahhhh@gmail.com>
2023-06-23 15:35:13 +0700

    Praktikum 14

--- perpustakaan/routes/web.php
+++ perpustakaan/routes/web.php
@@ -47,32 +47,36 @@ Route::get('/form-peminjaman-buku', [PeminjamanBukuController::class, 'index']);
 Route::post('/hasil-peminjaman', [PeminjamanBukuController::class, 'hasil']);
 
 //Praktikum 10
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
 //Tugas Praktikum 10
 //Route::get('/buku', [BukuController::class, 'buku']);
 
 //Praktikum 11
-Route::get('/dashboard/book', [BookController::class, 'index']);
+Route::get('/dashboard/book', [BookController::class, 'index'])->middleware('auth');
 //Tugas Praktikum 11
-Route::get('/dashboard/member', [MemberController::class, 'index']);
+Route::get('/dashboard/member', [MemberController::class, 'index'])->middleware('auth');
 
 //Praktikum 12 
-Route::get('/dashboard/book/create', [BookController::class, 'create']);
-Route::post('/dashboard/book/store', [BookController::class, 'store']);
-Route::delete('/dashboard/book/destroy/{id}', [BookController::class, 'destroy']);
+Route::get('/dashboard/book/create', [BookController::class, 'create'])->middleware('auth');
+Route::post('/dashboard/book/store', [BookController::class, 'store'])->middleware('auth');
+Route::delete('/dashboard/book/destroy/{id}', [BookController::class, 'destroy'])->middleware('auth');
 
 //Tugas Praktikum 12
-Route::get('/dashboard/member/create', [MemberController::class, 'create']);
-Route::post('/dashboard/member/store', [MemberController::class, 'store']);
-Route::delete('/dashboard/member/destroy/{id}', [MemberController::class, 'destroy']);
+Route::get('/dashboard/member/create', [MemberController::class, 'create'])->middleware('auth');
+Route::post('/dashboard/member/store', [MemberController::class, 'store'])->middleware('auth');
+Route::delete('/dashboard/member/destroy/{id}', [MemberController::class, 'destroy'])->middleware('auth');
 
 //Praktikum 13 - Edit dan Detail
-Route::get('/dashboard/book/edit/{id}', [BookController::class, 'edit']);
-Route::put('/dashboard/book/update/{id}', [BookController::class, 'update']);
-Route::get('/dashboard/book/show/{id}', [BookController::class, 'show']);
+Route::get('/dashboard/book/edit/{id}', [BookController::class, 'edit'])->middleware('auth');
+Route::put('/dashboard/book/update/{id}', [BookController::class, 'update'])->middleware('auth');
+Route::get('/dashboard/book/show/{id}', [BookController::class, 'show'])->middleware('auth');
 
 //Tugas Praktikum 13 - Edit dan Detail
-Route::get('/dashboard/member/edit/{id}', [MemberController::class, 'edit']);
-Route::put('/dashboard/member/update/{id}', [MemberController::class, 'update']);
-Route::get('/dashboard/member/show/{id}', [MemberController::class, 'show']);
+Route::get('/dashboard/member/edit/{id}', [MemberController::class, 'edit'])->middleware('auth');
+Route::put('/dashboard/member/update/{id}', [MemberController::class, 'update'])->middleware('auth');
+Route::get('/dashboard/member/show/{id}', [MemberController::class, 'show'])->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
